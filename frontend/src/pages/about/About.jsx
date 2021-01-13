c0bd6313fe75173400b2eaf71fd0e47e40fd5a6e
hwanfront <kim015jh@gmail.com>
2021-01-13 16:57:18 +0900

    refactor : Move src/About.jsx to src/pages/about/About.jsx

---
+++ frontend/src/pages/about/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container px-3">
+            <br/>
+                <h2>About</h2>
+            <br/>
+        </div>
+    );
+};
+export default About;
