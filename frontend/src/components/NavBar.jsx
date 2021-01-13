12f592be3312ef5453660446b327f6db637d8dd3
hwanfront <kim015jh@gmail.com>
2021-01-13 15:49:03 +0900

    fix : NavLink to Nav.Link
    - `<NavLink>` to `<Nav.Link>`

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavLink from 'react-bootstrap/NavLink';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 const NavBar = () => {
-
   return (
     <div>
-      <Navbar collapseOnSelect  bg="dark" expand="lg" variant="dark">
-        <Navbar.Brand href="/">&nbsp;금테코&nbsp;</Navbar.Brand>
+      <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark" sticky="top">
+        <Navbar.Brand href="/">&nbsp;금테코&nbsp;</Navbar.Brand>&nbsp;&nbsp;
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <NavLink href="/">Home</NavLink>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
             <NavDropdown title="Link" id="basic-nav-dropdown">
               <NavDropdown.Item href="https://www.acmicpc.net/" target="_blank">백준</NavDropdown.Item>
               <NavDropdown.Item href="https://solved.ac/" target="_blank">solved.ac</NavDropdown.Item>
               <NavDropdown.Item href="https://github.com/" target="_blank">GitHub</NavDropdown.Item>
             </NavDropdown>
-            {/* <NavLink href="/about">About</NavLink> */}
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -34,5 +27,4 @@ const NavBar = () => {
     </div>
   );
 }
-
 export default NavBar;
