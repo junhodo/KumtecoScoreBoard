672a73e349641d4b0ecd311733919865e786fcad
hwanfront <kim015jh@gmail.com>
2021-01-14 16:35:44 +0900

    feat : Add Progress bar, color events to scoreboard
    - fix background color in ScoreBoard.jsx
    - add progessbar in ScoreBoard.jsx
    - add spinner in ScoreBoard.jsx
    - fix dateInfo in info.jsx
    
    Resolves : #9

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ScoreBoard from './ScoreBoard';
 
-const Home = ( props ) => {
+const Home = () => {
     return (
         <div className="container px-3">
             <br />
             <h2>백준 스코어보드</h2>
             <br />
-            <ScoreBoard solvedData={props.solvedData}/>
+            <ScoreBoard />
         </div>
     );
 };
--- frontend/src/pages/home/info.jsx
+++ frontend/src/pages/home/info.jsx
@@ -1,7 +1,8 @@
 // ScroeBoard 테이블 정보
 
+
 export const userInfo = () => {
-    const data = [
+    return [
         { name: '김주현', id: 'darkraykim' },
         { name: '김정환', id: 'asdfg32145' },
         { name: '김해준', id: 'dubu4050' },
@@ -14,18 +15,34 @@ export const userInfo = () => {
         { name: '지명근', id: 'dldhk97' },
         // { name:'손우진', id:'lre12'},
     ];
-    return data;
 };
 
+
+const nowDate = new Date();
+
 export const dateInfo = () => {
-    const data = [
-        { first: '2020/12/28', second: '2021/01/03'},
-        { first: '2021/01/04', second: '2021/01/10'},
-        { first: '2021/01/11', second: '2021/01/17'},
-        { first: '2021/01/18', second: '2021/01/24'},
-        { first: '2021/01/25', second: '2021/01/31'},
-        { first: '2021/02/01', second: '2021/02/07'},
-        { first: '2021/02/08', second: '2021/01/14'},
-    ]
+    const data = [];
+    let firstDate = new Date('2020/12/28');
+    let secondDate = new Date('2021/01/03');
+    while(firstDate <= nowDate) {
+        const date = {
+            first: `${1900 + firstDate.getYear()}/${1 + firstDate.getMonth()}/${firstDate.getDate()}`,
+            second: `${1900 + secondDate.getYear()}/${1 + secondDate.getMonth()}/${secondDate.getDate()}`,
+        }
+        firstDate.setDate(firstDate.getDate() + 7);
+        secondDate.setDate(secondDate.getDate() + 7);
+        data.push(date);
+    }
+
+    // const data = [
+    //     { first: '2020/12/28', second: '2021/01/03'},
+    //     { first: '2021/01/04', second: '2021/01/10'},
+    //     { first: '2021/01/11', second: '2021/01/17'},
+    //     { first: '2021/01/18', second: '2021/01/24'},
+    //     { first: '2021/01/25', second: '2021/01/31'},
+    //     { first: '2021/02/01', second: '2021/02/07'},
+    //     { first: '2021/02/08', second: '2021/02/14'},
+    // ];
+
     return data;
 }
