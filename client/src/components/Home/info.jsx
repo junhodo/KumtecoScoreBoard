69afca7caf45730c1ec4a0f5ace52ca06651775e
hwanfront <kim015jh@gmail.com>
2021-02-24 17:06:57 +0900

    refactor: 불필요한 데이터 제거

--- client/src/components/Home/info.jsx
+++ client/src/components/Home/info.jsx
@@ -10,7 +10,7 @@ export const userInfo = () => {
         { name: '이지우', id: 'juejue' },
         { name: '이지훈', id: 'leejihun' },
         { name: '지명근', id: 'dldhk97' },
-        // { name:'손우진', id:'lre12'},
+        // { name: '손우진', id: 'lre12' },
     ];
 };
 
@@ -18,8 +18,8 @@ export const userInfo = () => {
 export const dateInfo = () => {
     const data = [];
     const nowDate = new Date();
-    let firstDate = new Date('2020/12/28');
-    let secondDate = new Date('2021/01/03');
+    const firstDate = new Date('2020/12/28');
+    const secondDate = new Date('2021/01/03');
     while (firstDate <= nowDate) {
         const date = {
             first: `${1900 + firstDate.getYear()}/${1 + firstDate.getMonth()}/${firstDate.getDate()}`,
@@ -29,16 +29,6 @@ export const dateInfo = () => {
         secondDate.setDate(secondDate.getDate() + 7);
         data.push(date);
     }
-
-    // const data = [
-    //     { first: '2020/12/28', second: '2021/01/03'},
-    //     { first: '2021/01/04', second: '2021/01/10'},
-    //     { first: '2021/01/11', second: '2021/01/17'},
-    //     { first: '2021/01/18', second: '2021/01/24'},
-    //     { first: '2021/01/25', second: '2021/01/31'},
-    //     { first: '2021/02/01', second: '2021/02/07'},
-    //     { first: '2021/02/08', second: '2021/02/14'},
-    // ];
 
     return data;
 };
